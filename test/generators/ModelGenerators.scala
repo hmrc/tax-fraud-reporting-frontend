2df979f2c9b22e041c1473b61b878c09b6834235
shiromi <shiromi002@yahoo.co.uk>
2022-02-02 15:09:38 +0000

    RK-241 resolved merge conflicts

--- test/generators/ModelGenerators.scala
+++ test/generators/ModelGenerators.scala
@@ -57,15 +57,6 @@ trait ModelGenerators {
       )
     }
 
-  implicit lazy val arbitraryBusinessContactDetails: Arbitrary[BusinessContactDetails] =
-    Arbitrary {
-      for {
-        landlineNumber <- arbitrary[Option[String]]
-        mobileNumber   <- arbitrary[Option[String]]
-        email          <- arbitrary[Option[String]]
-      } yield BusinessContactDetails(landlineNumber, mobileNumber, email)
-    }
-
   implicit lazy val arbitraryBusinessInformationCheck: Arbitrary[BusinessInformationCheck] =
     Arbitrary {
       Gen.oneOf(BusinessInformationCheck.values)
