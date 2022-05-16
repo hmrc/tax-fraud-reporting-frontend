93406fb661d386738302f0fdb8072a6ccb34a483
shinealex33 <78915315+shinealex33@users.noreply.github.com>
2022-05-16 09:52:30 +0100

    RK-coverage fix - fixing the coverage

--- app/models/addresslookup/Address.scala
+++ app/models/addresslookup/Address.scala
@@ -39,51 +39,8 @@ case class Address(
   @JsonIgnore // needed because the name starts 'is...'
   def isValid: Boolean = lines.nonEmpty && lines.size <= (if (town.isEmpty) 4 else 3)
 
-  def nonEmptyFields: List[String] = lines ::: town.toList ::: county.toList ::: List(postcode)
-
-  /** Gets a conjoined representation, excluding the country. */
-  def printable(separator: String): String = nonEmptyFields.mkString(separator)
-
-  /** Gets a single-line representation, excluding the country. */
-  @JsonIgnore // needed because it's a field
-  lazy val printable: String = printable(", ")
-
-  def line1: String = if (lines.nonEmpty) lines.head else ""
-
-  def line2: String = if (lines.size > 1) lines(1) else ""
-
-  def line3: String = if (lines.size > 2) lines(2) else ""
-
-  def line4: String = if (lines.size > 3) lines(3) else ""
-
-  def longestLineLength: Int = nonEmptyFields.map(_.length).max
-
-  def truncatedAddress(maxLen: Int = maxLineLength): Address =
-    Address(
-      lines.map(limit(_, maxLen)),
-      town.map(limit(_, maxLen)),
-      county.map(limit(_, maxLen)),
-      postcode,
-      subdivision,
-      country
-    )
-
 }
 
 object Address {
-  implicit val formats        = Json.format[Address]
-  val maxLineLength           = 35
-  val danglingLetter: Pattern = Pattern.compile(".* [A-Z0-9]$")
-
-  private def limit(str: String, max: Int): String = {
-    var s = str
-    while (s.length > max && s.indexOf(", ") > 0) s = s.replaceFirst(", ", ",")
-    if (s.length > max) {
-      s = s.substring(0, max).trim
-      if (Address.danglingLetter.matcher(s).matches())
-        s = s.substring(0, s.length - 2)
-      s
-    } else s
-  }
-
+  implicit val formats = Json.format[Address]
 }
